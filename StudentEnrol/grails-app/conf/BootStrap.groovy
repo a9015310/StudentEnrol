caf059c85b7546f0c16f74db07cf60d4f3aa09f5
a9015310 <a9015310@my.shu.ac.uk>
2015-02-02 16:48:44 +0000

    implemented scaffolding and bootstrapping

--- StudentEnrol/grails-app/conf/BootStrap.groovy
+++ StudentEnrol/grails-app/conf/BootStrap.groovy
@@ -17,6 +17,19 @@ course1.errors.allErrors.each{ error ->
 println "An error occurred whilst creating course1: ${error}"
 }
 }
+def course2= new Course( title: 'Digital Technologies',
+department: 'Computing',
+description: ''' Lorems ipsum dolor sit amet, ad sea solum 
+brute sensibus, te summor senserit vix, mel reque corpora vulputate eit.''',
+leader: 'Joe Jojo',
+code: 'IMA102',
+startDate: new Date('19/01/2015'),
+endDate: new Date('19/01/2020'))
+if(!course2.save()) {
+course2.errors.allErrors.each{ error ->
+println "An error occurred whilst creating course2: ${error}"
+}
+}
 
     }
     def destroy = {
